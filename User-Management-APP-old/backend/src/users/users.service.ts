d595f729532acc2db4feccd9608c39dc926518db
YunusPanwar70 <yunuspanwar78@gmail.com>
2025-05-10 13:37:46 +0530

    Make User Management App

---
+++ User-Management-APP-old/backend/src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
+
+  create(user: Partial<User>) {
+    return this.userRepository.save(user)
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id })
+  }
+
+  update(id: number, user: Partial<User>) {
+    return this.userRepository.update(id, user)
+  }
+
+  delete(id: number) {
+    return this.userRepository.delete(id)
+  }
+}
